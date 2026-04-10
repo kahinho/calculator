cddf3cfdb3f3ae4edb3d266f31651566595f1f7c
ansari <ansari.abdirahman@hotmail.com>
2026-04-10 03:09:26 +0200

    feat: calculating the area and the perimeter of a rectangle using dynamic input with cin

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int main() {
-    cout << "This is my first c++ line of code." << endl;
+    int rectangle_length, rectangle_width;
+    int rectangle_area, rectangle_perimeter;
+    cout << "Please provide the length of the rectangle" << endl;
+    cin >> rectangle_length;
+    cout << "The length of the rectangle is " << rectangle_length<<endl;
+    cout << "Please provide the width of the rectangle" << endl;
+    cin >> rectangle_width;
+    cout << "The width of the rectangle is " << rectangle_width<<endl;
+    rectangle_area = rectangle_length * rectangle_width;
+    cout << "The area of the rectangle is " << rectangle_area<<endl;
+    rectangle_perimeter = (rectangle_length + rectangle_width)*2;
+    cout << "The perimeter of the rectangle is " << rectangle_perimeter<<endl;
     return 0;
 }
