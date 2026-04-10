292f40092f3c76162ca81fb64f253a06594d3bed
ansari <ansari.abdirahman@hotmail.com>
2026-04-10 03:12:01 +0200

    feat: calculating the area and the perimeter of a square using dynamic input with cin

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     int rectangle_length, rectangle_width;
     int rectangle_area, rectangle_perimeter;
+    int square_length, square_area, square_perimeter;
     cout << "Please provide the length of the rectangle" << endl;
     cin >> rectangle_length;
     cout << "The length of the rectangle is " << rectangle_length<<endl;
@@ -14,5 +15,14 @@ int main() {
     cout << "The area of the rectangle is " << rectangle_area<<endl;
     rectangle_perimeter = (rectangle_length + rectangle_width)*2;
     cout << "The perimeter of the rectangle is " << rectangle_perimeter<<endl;
+
+    cout << "Please provide the length of the square" << endl;
+    cin >> square_length;
+    cout << "The length of the square is " << square_length<<endl;
+    square_area = square_length * square_length;
+    cout << "The area of the square is " << square_area <<endl;
+    square_perimeter = square_length*4;
+    cout << "The perimeter of the square is " << square_perimeter <<endl;
+    cout << "The perimeter of the square is: " << square_perimeter << endl;
     return 0;
 }
